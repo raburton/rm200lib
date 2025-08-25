6aa098506128deece8f3badf5be6f6f368423730
Richard Antony Burton <richardaburton@gmail.com>
2025-08-25 18:05:47 +0100

    more functions

--- rm200lib.py
+++ rm200lib.py
@@ -71,6 +71,99 @@ def FileDir():
 def FileDelete(file):
     return command_bool(b"\x77\x25" + file.encode('utf8') + b'\0')
 
+def EnterBootloader():
+    return command_bool(b'\x78\x10\x87\xef\x3a\x1a')
+
+def GetDeviceMode():
+    # 1=eGeneral, 2=eBatteryOnly, 3=eSync, 4=eRemote, 5=eTukan, 6=eBatteryPowered, 9=eMSD
+    ret = command(b'\x78\x2a')
+    if (len(ret) == 1):
+        return ret[0]
+    else:
+        return None
+
+def SetDeviceMode(mode):
+    # 1=eGeneral, 2=eBatteryOnly, 3=eSync, 4=eRemote, 5=eTukan, 6=eBatteryPowered, 9=eMSD
+    # 2 will turn off the screen, puts to sleep?, wakes with differnet mode or key press
+    # 3 will display the teh sync image if on the flash as SyncMode.bmp
+    # 4 makes screen yellow!
+    if (mode < 1 or mode > 6) and mode != 9:
+        raise Exception('Mode must be 1=eGeneral, 2=eBatteryOnly, 3=eSync, 4=eRemote, 5=eTukan, 6=eBatteryPowered, 9=eMSD')
+    return command_bool(b'\x78\x29' + bytes([mode]))
+
+def GetAperture():
+    # returns 0=small, 1=medium, 2=large/auto
+    ret = command(b'\x78\x25')
+    if (len(ret) == 1):
+        return ret[0]
+    else:
+        return None
+
+def TriggerMeasurement(aperture):
+    if aperture < 0 or aperture > 2:
+        raise Exception('Aperture must be 0=small, 1=medium, or 2=large/auto')
+
+    return command_bool(b'\x78\x35' + bytes([aperture]))
+
+def Reboot():
+    return command_bool(b'\x77\x14')
+
+def BLUpload(file, action):
+
+    # todo check in bootloader mode
+    if action != 1 and action != 2 and action != 3 and action != 6:
+        raise Exception('Action must be 1=bootloader (dangerous!), 2=firmware, 3=calib, 6=welcome')
+
+    with open(file, "rb") as f:
+        data = f.read()
+
+    chunk_max = commsize - 40
+    offset = 0
+
+    while True:
+        chunk = data[offset : offset + chunk_max]
+
+        chunk_len = len(chunk)
+        if chunk_len == 0:
+            break
+
+        ret = BLUploadChunk(offset, chunk)
+        if ret == False:
+            return False
+        offset += chunk_size
+
+    print('Fnished upload, comitting...')
+
+    return BLAction(action, offset)
+
+def BLUploadChunk(offset, chunk):
+    chunk_len = len(chunk)
+    return command_bool(b"\x77\x12" + offset.to_bytes(4, "big") + chunk_len.to_bytes(4, "big") + chunk)
+
+def BLAction(action, size):
+    # write previously uploaded data to spi (bootloader) or appropriate nand location (firmware/calib/welcome bitmap)
+    # or in the case of action=6 size=0 erase the existing welcome bitmap
+    if action != 1 and action != 2 and action != 3 and action != 6:
+        raise Exception('Action must be 1=bootloader (dangerous!), 2=firmware, 3=calib, 6=welcome')
+
+    return command_bool(b'\x77\x13' + bytes([action]) + size.to_bytes(4, "big"))
+
+def BLUploadBootloader(file):
+    return BLUpload(file, 1)
+
+def BLUploadFirmware(file):
+    return BLUpload(file, 2)
+
+def BLUploadCalibration(file):
+    return BLUpload(file, 3)
+
+def BLUploadWelcome(file):
+    return BLUpload(file, 6)
+
+def BLEraseWelcome(file):
+    return BLAction(6, 0)
+
+
 def OpenFile(file, mode):
     if mode < 1 or mode > 2:
         raise Exception('Mode must be 1=read, 2=write')
