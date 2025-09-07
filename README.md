c0aad995a2a5546a05fb0e8e33a620445aaa68fe
Richard Antony Burton <richardaburton@gmail.com>
2025-09-07 15:33:21 +0100

    tidy up, add another small funciton

--- README.md
+++ README.md
@@ -9,19 +9,20 @@ For example, you can separatly open, read, and close a file on the device. Or yo
 Functions are generally named as per the log messages in the code (generally quite intuitive). To see what it can do, have a look at the comments in the code.
 
 Many functions return actual data (suitably interpreted, e.g. parsed and returned as a string array), not printed messages. For ease of reading you might want to
-wrap them in code to make them print out a little prettier, see example below.
+wrap them in code to make them print out a little prettier, see example below. Functions generally return data (or `None` on error) or `True`/`False` to indicate
+success (according to purpose of function). They will raise an exception if the device is not connected or there are parameter errors.
 
 To get started in python3:
 ```python
 import rm200lib as rm
 
-rm.connect()
+rm.Connect()
 
-rm.GetInfo() # return array of strings
+rm.GetInfo() # return array of device info strings
 print('\n'.join(rm.GetInfo())) # or pretty printed version
 
 rm.FileDir() # list files
 rm.DownloadFile('filename') # save file to same name in current dir
 
-rm.disconnect()
+rm.Disconnect()
 ```
--- rm200lib.py
+++ rm200lib.py
@@ -10,7 +10,7 @@ dev = None
 commsize = 140
 debug = False
 
-def connect():
+def Connect():
     global dev
     dev = usb.core.find(idVendor=0x0765, idProduct=0x6001)
     if dev is None:
@@ -22,12 +22,13 @@ def connect():
 
     GetComBufSize()
 
-def disconnect():
+def Disconnect():
     global dev
     if dev != None:
         usb.util.dispose_resources(dev)
         dev = None
 
+# enable some debugging in this code
 def SetDebug(enabled):
     global debug
     debug = enabled
@@ -36,19 +37,20 @@ def GetComBufSize():
     # remember this value for our use as well
     global commsize
 
-    ret = command(b'\x78\x11')
+    ret = CommandData(b'\x78\x11')
     if len(ret) == 4:
         commsize = int.from_bytes(ret, 'big')
         return commsize
     return None
 
+# allows use of the commands with Extended in the name, and 8823 (GetMultiColorCmd(true))
 def UnlockExtendedCommands(password):
     # multiple passwords, why?
-    # allows use of the commands with Extended in the name, and 8823 (GetMultiColorCmd(true))
     if password == None:
         password = '873gwe31xah1'
-    return command_bool(b"\x89\x00" + password.encode('utf8') + b'\0')
+    return CommandBool(b"\x89\x00" + password.encode('utf8') + b'\0')
 
+# gets various device info: serial, mfg date, device rev?, disk spcae total, used, free
 def GetInfo():
     # info is: serial num, mfg date, hw rev?, total disk space, used space, free space
     # special case, does own usb command as when called in bootloader it will send back
@@ -73,6 +75,7 @@ def GetInfo():
 
     return None
 
+# utility function to get the device serial number
 def GetSerialNum():
     info = GetInfo()
     if info == None:
@@ -82,7 +85,7 @@ def GetSerialNum():
 # gets array of colours, first the selected and then all scanned (inc selected again)
 # each colour is array of 5 strings: fandeck, colour, page, row, column
 def GetMultiColorCmd():
-    data = command(b"\x78\x23")
+    data = CommandData(b"\x78\x23")
 
     if data == None:
         return None
@@ -120,50 +123,56 @@ def GetMultiColorCmd():
 
     return colours
 
+# bootloader version (when running normal firmware)
 def GetBLInfo():
     #'2.41   Bootloader ' (null terminated)
-    bin = command(b'\x78\x2d')
+    bin = CommandData(b'\x78\x2d')
     if bin == None:
         return None
     return str(bin[:-1], 'utf8')
 
+# current running firmware (or bootloader if that's running)
 def GetFWInfo():
     #'2.16   RM200' (null terminated)
     #'2.16    RM200 Cosmetics' (null terminated)
-    bin = command(b'\x77\x01')
+    bin = CommandData(b'\x77\x01')
     if bin == None:
         return None
     return str(bin[:-1], 'utf8')
 
+# the chip id/ security id, used when syncing with the server
 def GetChipId():
-    bin = command(b'\x78\x07')
+    bin = CommandData(b'\x78\x07')
     if bin == None:
         return None
     return '0x' + bytes(bin).hex()
 
 def GetDeltaEParameter():
     # also has a set method, function unknown, 5 little endian ints
-    bin = command(b'\x78\x37')
+    bin = CommandData(b'\x78\x37')
     if len(bin) == 20:
         return [int.from_bytes(bin[0:4], 'little'), int.from_bytes(bin[4:8], 'little'), int.from_bytes(bin[8:12], 'little'),
                 int.from_bytes(bin[12:16], 'little'), int.from_bytes(bin[16:20], 'little')]
     else:
         return None
 
+# get a directory listing
 def FileDir():
-    dat = command(b'\x77\x24')
+    data = CommandData(b'\x77\x24')
     # 32bit int (string count), then array of strings null terminated/separated
-    return str(dat[4:-1], 'utf8').split('\0')
+    return str(data[4:-1], 'utf8').split('\0')
 
 def FileDelete(file):
-    return command_bool(b"\x77\x25" + file.encode('utf8') + b'\0')
+    return CommandBool(b"\x77\x25" + file.encode('utf8') + b'\0')
 
+# reboto to bootloader
 def EnterBootloader():
-    return command_bool(b'\x78\x10\x87\xef\x3a\x1a')
+    return CommandBool(b'\x78\x10\x87\xef\x3a\x1a')
 
+# significance of this not really clear
 def GetDeviceMode():
     # 1=eGeneral, 2=eBatteryOnly, 3=eSync, 4=eRemote, 5=eTukan, 6=eBatteryPowered, 9=eMSD
-    ret = command(b'\x78\x2a')
+    ret = CommandData(b'\x78\x2a')
     if (len(ret) == 1):
         return ret[0]
     else:
@@ -176,11 +185,11 @@ def SetDeviceMode(mode):
     # 4 makes screen yellow!
     if (mode < 1 or mode > 6) and mode != 9:
         raise Exception('Mode must be 1=eGeneral, 2=eBatteryOnly, 3=eSync, 4=eRemote, 5=eTukan, 6=eBatteryPowered, 9=eMSD')
-    return command_bool(b'\x78\x29' + bytes([mode]))
+    return CommandBool(b'\x78\x29' + bytes([mode]))
 
 # returns array containing int percentage???, float voltage, int mode (0=charged, 2=charging, maybe 1=discharging?)
 def GetBatteryState():
-    data = command(b'\x79\x05')
+    data = CommandData(b'\x79\x05')
     if data == None or len(data) != 6:
         return None
     state = [data[0]]
@@ -231,8 +240,6 @@ def SetSerialNum(serial):
         raise Exception('Serial must be 10 digits long')
 
     return GenericCmd(0x032a, 0x00001d7e, 0x000005de, 0, 0, 0, 0, serial)
-    #return rm.command_bool(b'\x00\x00\x1d\x7e \x00\x00\x05\xde \x00\x00\x00\x00 \x00\x00\x00\x00 \x00\x00\x00\x00 \x00\x00\x00\x00' +
-    #       serial.encode('utf8') + b'\0')
 
 # Backup the calib data to a file on the nand.
 # Two readable text formats, and one binary dump (most useful for backup)
@@ -251,20 +258,27 @@ def BackupCalibData(mode):
 
 def GetAperture():
     # returns 0=small, 1=medium, 2=large/auto
-    ret = command(b'\x78\x25')
+    ret = CommandData(b'\x78\x25')
     if (len(ret) == 1):
         return ret[0]
     else:
         return None
 
+def SetAperture(aperture):
+    if aperture < 0 or aperture > 2:
+        raise Exception('Aperture must be 0=small, 1=medium, or 2=large/auto')
+    return CommandCool(b'\x78\x24' + bytes([aperture]))
+
+# take a sample (like fully pressing the side button)
 def TriggerMeasurement(aperture):
     if aperture < 0 or aperture > 2:
         raise Exception('Aperture must be 0=small, 1=medium, or 2=large/auto')
 
-    return command_bool(b'\x78\x35' + bytes([aperture]))
+    return CommandBool(b'\x78\x35' + bytes([aperture]))
 
+# reboot the device
 def Reboot():
-    return command_bool(b'\x77\x14')
+    return CommandBool(b'\x77\x14')
 
 def BLUpload(file, action):
 
@@ -296,7 +310,7 @@ def BLUpload(file, action):
 
 def BLUploadChunk(offset, chunk):
     chunk_len = len(chunk)
-    return command_bool(b"\x77\x12" + offset.to_bytes(4, "big") + chunk_len.to_bytes(4, "big") + chunk)
+    return CommandBool(b"\x77\x12" + offset.to_bytes(4, "big") + chunk_len.to_bytes(4, "big") + chunk)
 
 def BLAction(action, size):
     # write previously uploaded data to spi (bootloader) or appropriate nand location (firmware/calib/welcome bitmap)
@@ -304,41 +318,51 @@ def BLAction(action, size):
     if action != 1 and action != 2 and action != 3 and action != 6:
         raise Exception('Action must be 1=bootloader (dangerous!), 2=firmware, 3=calib, 6=welcome')
 
-    return command_bool(b'\x77\x13' + bytes([action]) + size.to_bytes(4, "big"))
+    return CommandBool(b'\x77\x13' + bytes([action]) + size.to_bytes(4, "big"))
 
+# utility function to upload a new bootloader - dangerous!
+# see Ivor Hewitts's blog on how to recover if this goes badly
 def BLUploadBootloader(file):
     return BLUpload(file, 1)
 
+# utility function to upload a new firmware
 def BLUploadFirmware(file):
     return BLUpload(file, 2)
 
+# utility function to upload new calibration data
 def BLUploadCalibration(file):
     return BLUpload(file, 3)
 
+# utility function to upload the boot image
 def BLUploadWelcome(file):
     return BLUpload(file, 6)
 
+# utility function to erase the boot image
 def BLEraseWelcome(file):
     return BLAction(6, 0)
 
 # get the current content of the screen, pixel data in RGB565, no headers
 def GetLcdData():
-    return command(b'\x78\x0e')
+    return CommandData(b'\x78\x0e')
 
+# open a file on the device
 def OpenFile(file, mode):
     if mode < 1 or mode > 2:
         raise Exception('Mode must be 1=read, 2=write')
 
-    return command_bool(b'\x77\x20' + bytes([mode]) + file.encode() + b'\0')
+    return CommandBool(b'\x77\x20' + bytes([mode]) + file.encode() + b'\0')
 
+# read from a file, opened in read mode
 def FileRead():
-    return command(b"\x77\x22")
+    return CommandData(b"\x77\x22")
 
+# write to a file, opened in write mode
 def FileWrite(chunk, length):
-    return command_bool(b'\x77\x23' + length.to_bytes(4, "big") + chunk)
+    return CommandBool(b'\x77\x23' + length.to_bytes(4, "big") + chunk)
 
+# close the file when done (commits if writing)
 def CloseFile(file):
-    return command_bool(b'\x77\x21')
+    return CommandBool(b'\x77\x21')
 
 # upload data to a file on the device
 def PutFile(file, data):
@@ -491,24 +515,27 @@ def SaveScreenshot(file):
 # briefly display a picture on the screen
 # needs raw RBG565 data 176 x -220 pixels
 def Display565Image(file):
-    with open('lcd.dat', 'rb') as f:
+    with open(file, 'rb') as f:
         data = f.read()
     if len(data) != 77440:
         raise Exception('invalid RBG565 data, must be 77440 bytes')
-    return command_bool(command(b'\x79\x03' + data))
+    return CommandBool(b'\x79\x03' + data)
 
+# start previewing (like holding the side button half in)
 def StartPreview():
-    return command_bool(b'\x78\x34\x01')
+    return CommandBool(b'\x78\x34\x01')
 
+# stop previewing
 def StopPreview():
-    return command_bool(b'\x78\x34\x00')
+    return CommandBool(b'\x78\x34\x00')
 
 # get current preview image (device must be in preview mode, by button or command)
 # returns 2 byte width, 2 byte length, then pixel data in RGB565
 def GetPreview():
-    data = command(b'\x78\x16')
+    data = CommandData(b'\x78\x16')
     return data
 
+# utility function to save the preview image to a bmp file
 def SavePreview(file):
     header = bytes([
         0x42, 0x4d, 0x8a, 0xc8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x8a, 0x00, 0x00, 0x00, 0x7c, 0x00,
@@ -533,58 +560,60 @@ def SavePreview(file):
 
     return True
 
+# returns the temperature as a float
 def MeasureTemperature():
-    dat = command(b'\x78\x06')
-    if dat == None or len(dat) != 4:
+    data = CommandData(b'\x78\x06')
+    if data == None or len(data) != 4:
         return None
-    return struct.unpack('>f', dat)[0]
+    return struct.unpack('>f', data)[0]
 
 # gets the time as an array of values: year, month, day, hours, mins, secs
 def GetTime():
-    dat = command(b'\x97\x0a')
-    if dat == None or len(dat) != 7:
+    data = CommandData(b'\x97\x0a')
+    if data == None or len(data) != 7:
         return None
-    return [int.from_bytes(dat[0:2]), dat[2], dat[3], dat[4], dat[5], dat[6]]
+    return [int.from_bytes(data[0:2]), data[2], data[3], data[4], data[5], data[6]]
 
 # utility function to get date as a formatted string
 def GetTimeString():
-    dat = GetTime()
-    if dat == None:
+    data = GetTime()
+    if data == None:
         return None
-    return f'{dat[0]}/{dat[1]}/{dat[2]} {dat[3]}:{dat[4]}:{dat[5]}'
+    return f'{data[0]}/{data[1]}/{data[2]} {data[3]}:{data[4]}:{data[5]}'
 
 def SetTime(year, month, day, hours, mins, secs):
-    return command_bool(b'\x79\x04' + year.to_bytes(2, 'big') + bytes([month]) + bytes([day]) +
+    return CommandBool(b'\x79\x04' + year.to_bytes(2, 'big') + bytes([month]) + bytes([day]) +
                         bytes([hours]) + bytes([mins]) + bytes([secs]))
 
+# send a key press event to the device, allowing remote control
 def GenerateKeyboardEvent(key):
     if key < 1 or key > 8:
         raise Exception('Key must be 1=centre, 2=up, 3=down, 4=left, 5=right, 6=preview(release), 7=preview(hold), 8=capture')
     # must be previewing before can use capture
-    return command_bool(b'\x78\x0f' + key.to_bytes(2, 'big'))
+    return CommandBool(b'\x78\x0f' + key.to_bytes(2, 'big'))
 
 # returns bitmask of current pressed keys
 # 0x1=up, 0x2=down, 0x4=left, 0x8=right, 0x10=centre, 0x20=???, 0x40=???, 0x80=power, 0x100=???
 # no preview or capture (return error)
 def GetKeyCode():
-    dat = command(b'\x97\x09')
-    if dat == None or len(dat) != 2:
+    data = CommandData(b'\x97\x09')
+    if data == None or len(data) != 2:
         return None
-    return int.from_bytes(dat, 'big')
+    return int.from_bytes(data, 'big')
 
 # get the number of saved colour records
 def GetNumberOfEntries():
-    dat = command(b'\x78\x19')
-    if dat == None or len(dat) != 2:
+    data = CommandData(b'\x78\x19')
+    if data == None or len(data) != 2:
         return None
-    return int.from_bytes(dat, 'big')
+    return int.from_bytes(data, 'big')
 
 # fetches the data of a saved sample
 # numbered from 0 to GetNumberOfEntries-1
 # returns array of 11 strings: date/time, fandeck, colour code, page, row, column, colour name, ??, page code, ??, ??
 #   and 1 byte array containing image in BGR565 (not RGB565)
 def GetRecordData(num):
-    data = command(b'\x78\x20' +  num.to_bytes(2, 'big'))
+    data = CommandData(b'\x78\x20' +  num.to_bytes(2, 'big'))
     if data == None:
         return None
 
@@ -643,8 +672,9 @@ def SaveRecordImage(record, file):
 
     return True
 
+# get array of fandecks on the device
 def GetFandecks():
-    data = command(b'\x78\x21')
+    data = CommandData(b'\x78\x21')
     if data == None:
         return None
 
@@ -683,30 +713,34 @@ def GetFandecks():
 
     return fandecks
 
+# Activate/deactivate/prioritise a fandeck
 def SetFandeckActive(name, state):
     if state < 0 or state > 2:
         raise Exception('State must be 0=disabled, 1=enabled, 2=priority')
-    return command_bool(b'\x78\x22' + name.encode('utf-16le') + b'\0\0' + bytes([state]))
+    return CommandBool(b'\x78\x22' + name.encode('utf-16le') + b'\0\0' + bytes([state]))
 
+# Delete a fandeck, you should deactivate it first and then reboot after.
 def DeleteFandeck(name):
-    # need to reboot afterwards
-    return command_bool(b'\x78\x32' + name.encode('utf-16le') + b'\0\0')
+    return CommandBool(b'\x78\x32' + name.encode('utf-16le') + b'\0\0')
 
 # number of second till device need calibtating again
 def GetTimeToCalibExpired():
-    dat = command(b'\x78\x2e')
-    if dat == None or len(dat) != 4:
+    data = CommandData(b'\x78\x2e')
+    if data == None or len(data) != 4:
         return None
-    return int.from_bytes(dat, 'big')
+    return int.from_bytes(data, 'big')
 
 # returns 0=not calibrated, 1=calibrated
 def GetCalibrationState():
-    dat = command(b'\x78\x28')
-    if dat == None or len(dat) != 1:
+    data = CommandData(b'\x78\x28')
+    if data == None or len(data) != 1:
         return None
-    return dat[0]
+    return data[0]
 
-def command(data):
+# Send a command, get data back (or None in case of error)
+# Pass the full command, including any data, as byte sequence
+# Will throw exception if not connected
+def CommandData(data):
     global dev
     global debug
 
@@ -728,7 +762,10 @@ def command(data):
 
     return None
 
-def command_bool(data):
+# Send a command and get a bool back to indicate success
+# Pass the full command, including any data, as byte sequence
+# Will throw exception if not connected
+def CommandBool(data):
     global dev
 
     if dev is None:
